159dca7f466fb940eb4ed986d40db32a9dc8c520
Jose Martin Cipriano <martincipriano@gmail.com>
2025-01-20 07:10:23 +0800

    Add the plugin declaration block and prevent direct access to the plugin.

---
+++ rest-api-manager.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Rest API Manager
+ * Description: A WordPress plugin to allow the users to toggle the REST API endpoints on and off.
+ * Author: Jose Martin Cipriano
+ * Version: 1.0.0
+ * Author URI: https://www.linkedin.com/in/jmcipriano
+ * License: GPL-2.0-or-later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+if (!defined('ABSPATH')) {
+  exit;
+}
